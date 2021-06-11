0dc7d4fc817fce19c614b71df69c585b89f96522
Max Roncace <me@caseif.net>
2021-06-11 01:15:53 -0400

    Add get_resource_meta definition to load.h; remove old unpack.h file

--- include/libarp/unpack/load.h
+++ include/libarp/unpack/load.h
@@ -4,6 +4,8 @@
 
 #include <stddef.h>
 
+int get_resource_meta(ConstArpPackage package, const char *path, arp_resource_meta_t *out_meta);
+
 int load_package_from_file(const char *path, ArpPackage *package);
 
 int load_package_from_memory(const unsigned char *data, size_t package_len, ArpPackage *package);
