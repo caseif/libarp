8ce891a923e4e836b05c82481113f5ad292ae11b
Max Roncace <me@caseif.net>
2021-06-18 00:41:35 -0400

    Add untracked file

---
+++ src/util/compress_stubs.c
@@ -0,0 +1,39 @@
+#include "internal/defines/misc.h"
+#include "internal/util/compress.h"
+#include "internal/util/error.h"
+
+#include <errno.h>
+
+#ifndef FEATURE_DEFLATE
+DeflateStream compress_deflate_begin(uint64_t total_input_bytes) {
+    libarp_set_error(DEFLATE_SUPPORT_ERROR);
+    errno = -1;
+    return NULL;
+}
+
+int compress_deflate(DeflateStream stream, void *data, size_t data_len, void **out_data, size_t *out_data_len) {
+    libarp_set_error(DEFLATE_SUPPORT_ERROR);
+    return -1;
+}
+
+void compress_deflate_end(DeflateStream stream) {
+    libarp_set_error(DEFLATE_SUPPORT_ERROR);
+    errno = -1;
+}
+
+DeflateStream decompress_deflate_begin(uint64_t total_input_bytes, uint64_t total_output_bytes) {
+    libarp_set_error(DEFLATE_SUPPORT_ERROR);
+    errno = -1;
+    return NULL;
+}
+
+int decompress_deflate(DeflateStream stream, void *in_data, size_t in_data_len, void **out_data, size_t *out_data_len) {
+    libarp_set_error(DEFLATE_SUPPORT_ERROR);
+    return -1;
+}
+
+void decompress_deflate_end(DeflateStream stream) {
+    libarp_set_error(DEFLATE_SUPPORT_ERROR);
+    errno = -1;
+}
+#endif
