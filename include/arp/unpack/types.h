1605612eb2d10a4a021b9edfaa25bc0034922e94
Max Roncace <me@caseif.net>
2022-05-01 19:13:49 -0400

    Move ArpPackageMeta struct to existing unpack/types.h file

--- include/arp/unpack/types.h
+++ include/arp/unpack/types.h
@@ -12,6 +12,20 @@ typedef const void *ConstArpPackage;
 typedef void *ArpPackageSet;
 typedef const void *ConstArpPackageSet;
 
+typedef struct ArpPackageMeta {
+    uint16_t major_version;
+    char compression_type[PACKAGE_COMPRESSION_LEN + 1];
+    char package_namespace[PACKAGE_NAMESPACE_LEN + 1];
+    uint16_t total_parts;
+    uint64_t cat_off;
+    uint64_t cat_len;
+    uint32_t node_count;
+    uint32_t directory_count;
+    uint32_t resource_count;
+    uint64_t body_off;
+    uint64_t body_len;
+} arp_package_meta_t;
+
 typedef struct ArpResourceMeta {
     ArpPackage package;
     char *base_name;
